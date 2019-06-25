2c23d9d0bf164ba538b8e219137e45dad7b8b80a
hasan <hassanhitleap1@gmail.com>
2019-06-25 12:18:12 +0300

    lesson  21

--- public/index.php
+++ public/index.php
@@ -1,12 +1,38 @@
 <?php
 
+/**
+ * Front controller
+ *
+ * PHP version 5.4
+ */
 
-require "../Core/Route.php";
- $route = new Route();
+/**
+ * Routing
+ */
+require '../Core/Router.php';
 
-$route->add('',['controller'=>'Home','action'=>'index']);
-$route->add('/post',['controller'=>'Post','action'=>'index']);
-$route->add('Post/show',['controller'=>'Home','action'=>'show']);
-var_dump($route->getRoute());
-echo "<br/>";
-echo "thr requser url =" .$_SERVER['QUERY_STRING'];
+$router = new Router();
+
+// Add the routes
+//$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+    
+// Display the routing table
+echo '<pre>';
+//var_dump($router->getRoutes());
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
+
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
